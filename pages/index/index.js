d51e5e7fb97cd0ffbd7bcd0552abaca9aff1cb62
coconut-palm <969606616@qq.com>
2020-02-14 20:57:15 +0800

    2020-2-14

---
+++ pages/index/index.js
@@ -0,0 +1,56 @@
+import {request} from "../../request/index.js";
+
+//Page Object
+Page({
+  data: {
+    // 轮播图数组
+    swiperList:[]
+  },
+  // 页面开始加载 就会触发
+  onLoad: function(options) {
+    // 发送异步请求获取轮播图数据
+    // wx.request({
+    //   url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+    //   success: (result) => {
+    //     this.setData({
+    //       swiperList:result.data.message
+    //     })
+    //   }
+    // });
+
+    request({url:"https://api.zbztb.cn/api/public/v1/home/swiperdata"}).then(result=>{
+      this.setData({
+        swiperList:result.data.message
+      })
+    })
+  },
+  onReady: function() {
+    
+  },
+  onShow: function() {
+    
+  },
+  onHide: function() {
+
+  },
+  onUnload: function() {
+
+  },
+  onPullDownRefresh: function() {
+
+  },
+  onReachBottom: function() {
+
+  },
+  onShareAppMessage: function() {
+
+  },
+  onPageScroll: function() {
+
+  },
+  //item(index,pagePath,text)
+  onTabItemTap:function(item) {
+
+  }
+});
+  
