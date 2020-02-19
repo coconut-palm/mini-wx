f0d98d11c1108e97178fe25f4675ef7ffdbabcfb
coconut-palm <969606616@qq.com>
2020-02-19 21:53:51 +0800

    2020-2-19

--- request/index.js
+++ request/index.js
@@ -1,4 +1,12 @@
+// 同时发送异步代码的次数
+let ajaxTimes=0;
 export const request=(params) => {
+  ajaxTimes++;
+  // 显示加载中 效果
+  wx.showLoading({
+    title: "加载中",
+    mask: true
+  });
   // 定义公共的url
   const baseUrl="https://api.zbztb.cn/api/public/v1";
   return new Promise((resolve,reject)=> {
@@ -10,7 +18,14 @@ export const request=(params) => {
       },
       fail:(err)=>{
         reject(err)
-      }
+      },
+      complete:()=>{
+        ajaxTimes--;
+        if(ajaxTimes===0){
+          //  关闭正在等待的图标
+          wx.hideLoading();
+        }
+       }
     });
       
   })
